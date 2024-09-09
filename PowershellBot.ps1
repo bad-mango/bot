9617bc0e7e7f3b7373fb68d91a7aab836d433183
bad-mango <Vulchemar.t@gmail.com>
2024-09-09 18:07:15 +0200

    Update PowershellBot.ps1

--- PowershellBot.ps1
+++ PowershellBot.ps1
@@ -1,5 +1,4 @@
 # Define hardcoded token, channelId, botUserId, and baseUrl
-$channelId = "1282731955355648104"
 $botUserId = "1282419666022563975"  # Hardcoded bot user ID
 $baseUrl = "https://discord.com/api/v10"
 
